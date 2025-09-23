287e449e6611b13d3dea3c5e95f092c973bf2d55
Meissa Gningue <99593751+r00tMG@users.noreply.github.com>
2025-09-23 07:13:19 +0000

    add function staging area

--- dags/update_db.py
+++ dags/update_db.py
@@ -1,20 +1,38 @@
 import json
-from datetime import datetime
+import os
+from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 
 def read_json():
-    DIR_FILE="./files"
-    filename="{DIR_FILE}/data_youtube_extract.json"
-    with open(filename, "r", encoding="utf-8") as f:
-        datas=json.load(f)
-    return datas
+    try:
+        DIR_FILE="./files"
+        filename=os.path.join(DIR_FILE,"data_youtube_extract.json")
+        if filename and os.path.exists(filename):
+            with open(filename, "r", encoding="utf-8") as f:
+                datas=json.load(f)
+            return datas
+        else:
+            raise FileNotFoundError(f"Le fichier {filename} est introuvable")
+    except Exception as e:
+        raise Exception("Erreur lors de la lecture du fichier json dans staging area")
+
+def staging_area():
+    
+    
         
 
 with DAG(
     dag_id="update_db",
     description="chargement, transformation des données",
     start_date=datetime(2025, 1, 1),
-    scheduler="@daily",
+    schedule="@daily",
     catchup=False
 ) as dag:
-    pass
+    read_json_task = PythonOperator(
+        task_id="read_json",
+        python_callable=read_json,
+        retries=3,
+        retry_delay=timedelta(minutes=5)
+    )
+    read_json_task

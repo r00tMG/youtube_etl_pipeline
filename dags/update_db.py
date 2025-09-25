b067aef2cda55ffc731111e2bf3167866e4c5f30
r00tMG <meissagningue7@gmail.com>
2025-09-25 18:05:47 +0100

    drop /logs

--- dags/update_db.py
+++ dags/update_db.py
@@ -2,14 +2,15 @@ import json
 import os, sys
 from datetime import datetime, timedelta
 
-from fastapi import Depends
-from sqlalchemy.orm import Session
-
+import isodate
 from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
+from sqlalchemy import MetaData, Table, Column, String, Integer, DateTime, JSON, create_engine
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from app.database import SessionLocal
+from app.database import SessionLocal, DATABASE_URL
 from app.models import StagingYoutubeData
+
 
 def read_json():
     try:
@@ -46,13 +47,72 @@ def staging_area():
     else:
         raise Exception("La lecture du fichier json a soit échoué ou retourne un objet vide dans le staging area: ",datas)
 
+
+def parse_duration(value):
+    """Parse ISO8601 ou hh:mm:ss string"""
+    try:
+        # Cas 1: ISO8601 (PTxxHxxMxxS)
+        return isodate.parse_duration(value)
+    except Exception:
+        # Cas 2: format hh:mm:ss ou mm:ss
+        parts = value.split(":")
+        if len(parts) == 2:  # mm:ss
+            minutes, seconds = map(int, parts)
+            return timedelta(minutes=minutes, seconds=seconds)
+        elif len(parts) == 3:  # hh:mm:ss
+            hours, minutes, seconds = map(int, parts)
+            return timedelta(hours=hours, minutes=minutes, seconds=seconds)
+        else:
+            raise ValueError(f"Durée invalide: {value}")
+
 def transformation_and_clean():
     db=SessionLocal()
     datas = db.query(StagingYoutubeData).all()
     channel_handle=datas[0].channel_handle
     extraction_date=datetime.fromisoformat(datas[0].extraction_date).date()
-    for video in datas[0].videos:
-        pass
+    seen_ids = set()
+    clean_videos = []
+
+    for video in datas[0].videos:  # vidéos déjà list/dict
+        video_id = video["video_id"]
+
+        if video_id in seen_ids:
+            print(f"Vidéo {video_id} déjà présente, skipping...")
+            continue
+
+        seen_ids.add(video_id)
+
+        clean_videos.append({
+            "title": video["title"],
+            "duration": parse_duration(video["duration"]),
+            "video_id": video_id,
+            "like_count": int(video["like_count"]),
+            "view_count": int(video["view_count"]),
+            "published_at":datetime.fromisoformat(video["published_at"]).date(),
+            "comment_count":int(video["comment_count"]),
+            "duration_readable":parse_duration(video["duration_readable"])
+        })
+    return {
+        "channel_handle": channel_handle,
+        "extraction_date": extraction_date,
+        "total_videos":datas[0].total_videos,
+        "videos": clean_videos
+    }
+
+def load_core():
+    engine = create_engine(DATABASE_URL)
+    metadata = MetaData()
+    transform_datas = transformation_and_clean()
+    youtube_videos = Table(
+        "youtube_datas",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("channel_handle", String),
+        Column("extraction_date", DateTime),
+        Column("total_videos", Integer),
+        Column("videos", JSON)
+    )
+    metadata.create_all(engine)
 
 with DAG(
     dag_id="update_db",
